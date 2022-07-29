2ec7518c4c5520beccf3a6ece1f13b3e568e1a45
Ales Zima <zima.ales@seznam.cz>
2022-07-29 12:21:32 +0200

    Point calculator UI changes

--- Assets/Scripts/Table/PointCalculator.cs
+++ Assets/Scripts/Table/PointCalculator.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using System.Linq;
 using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class PointCalculator : MonoBehaviour
 {
     [SerializeField] private IntVariable scoreAsset = null;
     [SerializeField] private int tableMultiplier = 1;
     [SerializeField] private GameConfig config = null;
+    [SerializeField] private Image loadingBar = null;
+    [SerializeField] private GameObject pointsGainedText = null;
+
+    [SerializeField] private Color32 negativePointsColor = Color.red;
+    [SerializeField] private Color32 positivePointsColor = Color.green;
 
     private bool isResolving = false;
     private float timeWaited = 0f;
+    private float timeToWait = 0f;
+    private int scoreToAdd = 0;
 
     private TableManager tableManager = null;
 
@@ -28,22 +37,55 @@ public class PointCalculator : MonoBehaviour
             }
         }
 
-        int _scoreToAdd = config.TableRewards[matches] * tableMultiplier;
-        float _timeToWait = tableMultiplier * config.BaseTableTime * 0.75f; // multiplied by 0.75 to better suit wait times for higher multipliers
-        StartCoroutine(DelayResolve(_timeToWait, _scoreToAdd));
+        scoreToAdd = config.TableRewards[matches] * tableMultiplier;
+        timeToWait = tableMultiplier * config.BaseTableTime * 0.75f; // multiplied by 0.75 to better suit wait times for higher multipliers
+        StartResolving();
         
     }
 
-    private IEnumerator DelayResolve(float timeToWait, int scoreValue)
+    private void StartResolving() 
     {
-        yield return new WaitForSeconds(timeToWait);
-        scoreAsset.AddValue(scoreValue);
-        tableManager.ResetTable();
+        isResolving = true;
+        loadingBar.gameObject.SetActive(true);
     }
 
-
-    private void Resolve(float timeToWait)
+    private void Update() 
     {
+        if(isResolving)
+        {
+            timeWaited += Time.deltaTime;
+            float _progress = timeWaited / timeToWait;
+            loadingBar.fillAmount = _progress;
 
+            if(timeWaited >= timeToWait) Resolve();
+        }
+    }
+
+    private void Resolve()
+    {
+        isResolving = false;
+        timeWaited = 0f;
+        scoreAsset.AddValue(scoreToAdd);
+        tableManager.ResetTable();
+        loadingBar.gameObject.SetActive(false);
+
+        if(pointsGainedText != null) StartCoroutine(ShowPointsGained());
+    }
+
+    private IEnumerator ShowPointsGained()
+    {
+        TextMeshProUGUI _textContent = pointsGainedText.GetComponent<TextMeshProUGUI>();
+
+        _textContent.text = scoreToAdd >= 0 ? "+" + scoreToAdd.ToString() : scoreToAdd.ToString();
+        _textContent.color = scoreToAdd >= 0 ? positivePointsColor : negativePointsColor;
+        pointsGainedText.SetActive(true);
+
+        yield return new WaitForSeconds(1.3f);
+
+        pointsGainedText.GetComponent<Animator>().SetTrigger("popOut");
+
+        yield return new WaitForSeconds(1.5f);
+
+        pointsGainedText.SetActive(false);
     }
 }
